cf99593222a5d7516d5bbfa6497e62e30b900843
pg964 <pg964@LAPTOP-GIJQHHEL>
2024-10-30 14:38:22 -0300

    Adicionar arquivos de projeto.

---
+++ Aula0300Exemplo/ContaBancaria.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula0300Exemplo
+{
+    public class ContaBancaria
+    {
+        public int Numero { get; set; }
+        public string Tipo { get; set; }
+        public string NomeCliente { get; set; }
+        public decimal LimiteCredito { get; set; }
+        private decimal Saldo;
+
+        public void ImprimirDados()
+        {
+            Console.WriteLine();
+            Console.WriteLine("====================");
+            Console.WriteLine("Dados Da Conta Bancaria");
+            Console.WriteLine("--------------------");
+            Console.WriteLine($"Numero Da Conta: {Numero}");
+            Console.WriteLine($"Tipo Da Conta: {Tipo}");
+            Console.WriteLine($"Nome Do Cliente: {NomeCliente}");
+            Console.WriteLine($"Limite De Credito: {LimiteCredito:c}");
+            Console.WriteLine($"Saldo: {Saldo:c}");
+            Console.WriteLine("====================");
+
+        }
+        public void Depositar(decimal valorDeposito) 
+        {
+            Saldo += valorDeposito;
+        }
+        // pega o valor que ja existe e somar mais o valor depositado
+        public void Sacar(decimal valorSaque) 
+        {
+
+            //Validação para conferir se o valor connfere
+          if (Saldo < valorSaque) 
+            {
+                Console.WriteLine("Não ha saldo suficiente");
+            }
+            else 
+            {
+                Saldo -= valorSaque;
+            }
+        }
+    }
+}
---
+++ Aula0300Exemplo/Program.cs
@@ -0,0 +1,87 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Aula0300Exemplo;
+using System.Data;
+
+//Instanciar um objeto do tipo conta bancaria
+ContaBancaria objCB1 = new ContaBancaria();
+//Atribuir valores para as propriedades objetos
+objCB1.Numero = 123456;
+objCB1.Tipo = "Conta corrente";
+objCB1.NomeCliente = "João da Silva";
+objCB1.LimiteCredito = 10000;
+
+
+
+
+
+
+//depositar dinheiro na conta 1
+objCB1.Depositar(150);
+objCB1.Depositar(678);
+objCB1.Depositar(432.65m);
+objCB1.Depositar(129);
+
+
+
+//Chamar o metodo imprimir dados do objCB1
+objCB1.ImprimirDados();
+
+//Criar nova conta
+
+ContaBancaria objCB2 = new ContaBancaria();
+objCB2.Numero = 678910;
+objCB2.Tipo = "Poupança";
+objCB2.NomeCliente = "Maria";
+objCB2.LimiteCredito = 20000;
+
+objCB2.Depositar(789);
+objCB2.Depositar(1000.01m);
+objCB2.Depositar(7560);
+
+objCB2.Sacar(349.01m);
+
+objCB2.ImprimirDados();
+
+
+//programa caixa eletronico
+
+ContaBancaria objCB3 =new ContaBancaria();
+objCB2.Numero = 101010;
+objCB2.Tipo = "Conta Corrente";
+objCB2.NomeCliente = "Anderson";
+objCB2.LimiteCredito = 5000;
+
+int opcao = 0;
+
+do 
+{
+    Console.WriteLine("Escolha uma opção:");
+    Console.WriteLine("1 - Depositar");
+    Console.WriteLine("2 - Sacar");
+    Console.WriteLine("3 - Imprimir Dados");
+    Console.WriteLine("0 - Sair");
+
+    opcao = Convert.ToInt32(Console.ReadLine());
+
+    switch (opcao) 
+    {
+        case 1:
+            Console.WriteLine("Digite o valor a depositar");
+            decimal vlDepositar = Convert.ToDecimal(Console.ReadLine());
+            objCB3.Depositar(vlDepositar);
+            break;
+
+        case 2:
+            Console.WriteLine("Digite o valor do Saque:");
+            decimal vlSaque = Convert.ToDecimal(Console.ReadLine());
+            objCB3.Sacar(vlSaque);
+            break;
+
+            case 3:
+            objCB3.ImprimirDados();
+            break;
+    }
+    Console.WriteLine("Aperte qualquer tecla para continuar....");
+    Console.ReadKey();
+    Console.Clear();
+} while (opcao!=0);
